a4bbf8d6260222fb46566388e5136a1d422f371d
ogabriel <gabrieloliver8991@gmail.com>
2019-07-02 00:28:26 -0300

    resolve rubocop corrections

--- db/migrate/20190702030445_create_books.rb
+++ db/migrate/20190702030445_create_books.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 class CreateBooks < ActiveRecord::Migration[5.2]
   def change
     create_table :books, id: :string do |t|
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
 #
@@ -12,11 +14,9 @@ george_martin = Author.create(name: 'George R. R. Martin', active: true)
 leonel_caldela = Author.create(name: 'Leonel Caldela', active: true)
 leonel_caldela = Author.create(name: 'Leonel Caldela', active: true)
 
-
 Book.create(title: 'Terceiro Deus', author: leonel_caldela)
 Book.create(title: 'O Crânio e o Corvo', author: leonel_caldela)
 Book.create(title: 'O Hobbit', author: tolkien)
 Book.create(title: 'O Senhor dos Anéis', author: tolkien)
 Book.create(title: 'O Silmarillion', author: tolkien)
 Book.create(title: 'A Song of Ice and Fire', author: george_martin)
-
