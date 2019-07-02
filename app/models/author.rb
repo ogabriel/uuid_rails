21f652d2a7642a9e8f8a26681f2916a5a9e24cee
ogabriel <gabrieloliver8991@gmail.com>
2019-07-02 01:17:18 -0300

    updating models with scopes

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,4 +1,13 @@
 # frozen_string_literal: true
 
 class Author < ApplicationRecord
+  has_many :books
+
+  scope :active, -> { where(active: true) }
+  # def self.active
+  #   where(active: true)
+  # end
+  scope :unactive, -> { where(active: false) }
+  # Author.unscoped
+  # default_scope { active }
 end
--- app/models/book.rb
+++ app/models/book.rb
@@ -2,4 +2,7 @@
 
 class Book < ApplicationRecord
   belongs_to :author
+
+  scope :active, -> { joins(:author).merge(Author.active)}
+  scope :unactive, -> { joins(:author).merge(Author.unactive)}
 end
