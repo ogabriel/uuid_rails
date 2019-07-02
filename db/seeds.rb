e41c58e58759c303c60804e439b6ceec53b5fd58
ogabriel <gabrieloliver8991@gmail.com>
2019-07-02 00:25:47 -0300

    add seeds to the project

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,18 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+leonel_caldela = Author.create(name: 'Leonel Caldela', active: true)
+tolkien = Author.create(name: 'J. R. R. Tolkien', active: false)
+george_martin = Author.create(name: 'George R. R. Martin', active: true)
+leonel_caldela = Author.create(name: 'Leonel Caldela', active: true)
+leonel_caldela = Author.create(name: 'Leonel Caldela', active: true)
+
+
+Book.create(title: 'Terceiro Deus', author: leonel_caldela)
+Book.create(title: 'O Crânio e o Corvo', author: leonel_caldela)
+Book.create(title: 'O Hobbit', author: tolkien)
+Book.create(title: 'O Senhor dos Anéis', author: tolkien)
+Book.create(title: 'O Silmarillion', author: tolkien)
+Book.create(title: 'A Song of Ice and Fire', author: george_martin)
+
